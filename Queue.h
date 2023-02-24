d36951191d32f7be6d774c53dd1ba9590d0e32c2
bsroawit1234 <sorawit.pha@student.mahidol.edu>
2023-02-24 16:01:50 +0700

    LAB

--- Queue.h
+++ Queue.h
@@ -1,27 +1,68 @@
-
-
 typedef struct {
-	 NodePtr headPtr,tailPtr;
+  NodePtr headPtr, tailPtr;
 	int size;
-}Queue;
+} Queue;
 
+void enqueue_struct(Queue* q, int x, int y){
+  NodePtr new_node = (NodePtr) malloc(sizeof(Node));
+  if(new_node) { 
+    /*Finish enqueue */
+    new_node->order_number = x;
+    new_node->qty = y;
+    new_node->nextPtr = NULL;
 
-void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+    if(!(q->headPtr)) q->headPtr = new_node;
+    else q->tailPtr->nextPtr = new_node;
+
+    q->tailPtr = new_node;
+    q->size += 1;
+  }
 }
 
 
 int dequeue_struct(Queue *q){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+  NodePtr t = q->headPtr;
+  int price;
+
+  if(q->size > 0) {
+    switch (t->order_number) {
+    case 1:
+      printf("Ramen\n");
+      price = 100;
+      break;
+    case 2:
+      printf("Somtum\n");
+      price = 20;
+      break;
+    case 3:
+      printf("KFC\n");
+      price = 50;
+      break;
+    default:
+      printf("No Food\n");
+      price = 0;
+      break;
+    }
+  }
+
+  if(t) {
+    int value = price * q->headPtr->qty;
+    q->headPtr = q->headPtr->nextPtr;
+    if(!(q->headPtr)) q->tailPtr = NULL;
+    free(t);
+    q->size -= 1;
+    return value;
+  }
+  printf("The queue is empty\n");
+  return 0;
 }
 
+int count_queue(Queue *q) {
+  NodePtr temp = q->headPtr;
+  int count = 0;
+  while(temp != NULL) {
+    count++;
+    temp = temp->nextPtr;
+  }
+  return count;
+}
--- main.c
+++ main.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Node.h"
 
+#include "Node.h"
 #include "Queue.h"
 
-int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
+int main(int argc, char **argv) {
+  // NodePtr headPtr = NULL;
+  // NodePtr tailPtr = NULL;
+// For struct Queue
   Queue  q;
-   q. headPtr=NULL;
-   q.tailPtr=NULL;
-   q.size=0;
-*/
-   int i,x;
-   
+  q.headPtr=NULL;
+  q.tailPtr=NULL;
+  q.size=0;
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
+  int i, x, customer=0;
+
+  for(i = 1; i < argc; i++){
+    if(strcmp(argv[i], "x") == 0){
+      int cash;
+      printf("Customer no: %d\n", ++customer);
+      x = dequeue_struct(&q);
+
+      if(x != 0) {
+        printf("You have to pay %d\n", x);
+        do {
+          printf("Cash: ");
+          scanf("%d", &cash);
+        } while(cash < x);
+
+        printf("Thank you\n");
+        if(cash > x) {
+          printf("Change is: %d\n", cash-x);
         }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
-        }
- }
+      } else {
+        printf("Thank you\n");
+      }
+    } else {
+      // enqueue(&headPtr, &tailPtr, atoi(argv[i]));
+      enqueue_struct(&q, atoi(argv[i]), atoi(argv[i+1]));
+      printf("My order is %d\n", atoi(argv[i]));
+      i++;
+    }
+  }
+
+  printf("==============================\n");
+  int count_q = count_queue(&q);
+  if(count_q == 1) {
+    printf("There is %d ppl left in the queue.", count_q);
+  } else if(count_q > 1) {
+    printf("There are %d ppl left in the queue.", count_q);
+  }
+
   return 0;
 }
